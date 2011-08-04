0f7e32cc7772e47a3d851f279a5e499606781173
Hongli Lai (Phusion) <hongli@phusion.nl>
2011-08-04 22:06:11 +0200

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Juvia::Application.config.secret_token = '7c09a74e53a97a2ebf5328ce5333e6634502931587ce75d2fefc36aa5b9427d71f31645bd4e6001118f0673aec991caec9f61645538ec6fe4e6992c4aef501f8'
