44b78c12a98b3ddfd3488b0f4316b87ff46346b7
Hongli Lai (Phusion) <hongli@phusion.nl>
2011-08-13 20:48:38 +0200

    Don't make user registerable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable,
          :timeoutable
 
