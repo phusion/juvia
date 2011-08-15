d23c8f7d802dff68abb14cc396b190ac2a73c39a
Hongli Lai (Phusion) <hongli@phusion.nl>
2011-08-15 20:46:07 +0200

    Make jquery.delayed-observer more readable.

--- app/assets/javascripts/jquery.delayed-observer.js
+++ app/assets/javascripts/jquery.delayed-observer.js
@@ -11,24 +11,32 @@
 
 (function($){
     $.extend($.fn, {
-        delayedObserver: function(callback, delay, options){
-            return this.each(function(){
+        delayedObserver: function(callback, delay, options) {
+            return this.each(function() {
                 var el = $(this);
                 var op = options || {};
-                el.data('oldval', el.val())
-                    .data('delay', delay || 0.5)
-                    .data('condition', op.condition || function() { return ($(this).data('oldval') == $(this).val()); })
-                    .data('callback', callback)
-                    [(op.event||'keyup')](function(){
-                        if (el.data('condition').apply(el)) { return; }
-                        else {
-                            if (el.data('timer')) { clearTimeout(el.data('timer')); }
-                            el.data('timer', setTimeout(function(){
-                                el.data('callback').apply(el);
-                            }, el.data('delay') * 1000));
-                            el.data('oldval', el.val());
+                
+                el.data('oldval', el.val());
+                el.data('delay', delay || 0.5);
+                el.data('condition', op.condition || function() { return ($(this).data('oldval') == $(this).val()); });
+                el.data('callback', callback);
+                
+                function changeHandler() {
+                    if (el.data('condition').apply(el)) {
+                        return;
+                    } else {
+                        if (el.data('timer')) {
+                            clearTimeout(el.data('timer'));
                         }
-                    });
+                        var id = setTimeout(function() {
+                            el.data('callback').apply(el);
+                        }, el.data('delay') * 1000);
+                        el.data('timer', id);
+                        el.data('oldval', el.val());
+                    }
+                }
+                
+                el.bind(op.event || 'keyup', changeHandler);
             });
         }
     });
