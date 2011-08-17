dc361a7ae36899bc607a97b5d61a86f828ef8008
Hongli Lai (Phusion) <hongli@phusion.nl>
2011-08-17 17:34:00 +0200

    Fix some Spork issues.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,20 @@
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However,
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+end
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'spork'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -11,15 +23,10 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 Capybara.javascript_driver = :webkit
 DatabaseCleaner.logger = Rails.logger
 
-Spork.each_run do
-  ActiveSupport::Dependencies.clear
-end
-
 RSpec.configure do |config|
   config.include FactoryHelpers
   config.include SpecSupport
   config.include Devise::TestHelpers, :type => :controller
-  config.include Devise::TestHelpers, :type => :request
   
   # == Mock Framework
   #
