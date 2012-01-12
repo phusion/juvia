0da10e0b26154dbca0d1ba458781014fb8783b6a
Hongli Lai (Phusion) <hongli@phusion.nl>
2012-01-12 16:02:01 +0100

    Updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,30 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-user = User.create!({
-    :email => 'a@a.com',
-    :password => '123456',
-    :password_confirmation => '123456',
+if Rails.env.development?
+  user = User.create!({
+      :email => 'a@a.com',
+      :password => '123456',
+      :password_confirmation => '123456',
+      :admin => true
+    }, :as => :admin)
+  site = user.sites.create!({
+      :key => 'testkey',
+      :name => 'Test Site',
+      :url => 'http://test',
+      :user => user
+    }, :as => :admin)
+  site.update_attribute(:key, 'testkey')
+  topic = site.topics.create!(:key => 'testtopic', :title => 'Test Topic', :url => 'http://test/testtopic')
+  topic.comments.create!(:content => 'hello world',
+    :author_ip => '127.0.0.1',
+    :author_user_agent => 'Firefox',
+    :referer => 'http://www.google.com')
+else
+  User.create!({
+    :email => 'admin@localhost',
+    :password => 'admin',
+    :password_confirmation => 'admin',
     :admin => true
   }, :as => :admin)
-site = user.sites.create!({
-    :key => 'testkey',
-    :name => 'Test Site',
-    :url => 'http://test',
-    :user => user
-  }, :as => :admin)
-site.update_attribute(:key, 'testkey')
-topic = site.topics.create!(:key => 'testtopic', :title => 'Test Topic', :url => 'http://test/testtopic')
-topic.comments.create!(:content => 'hello world',
-  :author_ip => '127.0.0.1',
-  :author_user_agent => 'Firefox',
-  :referer => 'http://www.google.com')
+end
