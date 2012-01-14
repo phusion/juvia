255570fce1a160442c8cfc9595f89c533cd2db82
Hongli Lai (Phusion) <hongli@phusion.nl>
2012-01-15 00:25:59 +0100

    Precompile columnal.css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Juvia::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( columnal.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
