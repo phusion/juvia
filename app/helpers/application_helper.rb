7639f386b021018a281768dcb4b3b7e40a050961
Hongli Lai (Phusion) <hongli@phusion.nl>
2013-08-07 17:11:22 +0200

    Fix hiding the preview box when it the textarea is empty.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,12 +17,12 @@ module ApplicationHelper
   end
   
   def render_markdown(str)
-    html = BlueCloth.new(str, :escape_html => true, :strict_mode => false, :auto_links => true).to_html
+    html = BlueCloth.new(str, :pandoc_headers => false, :escape_html => true, :strict_mode => false, :auto_links => true).to_html
     noko = Nokogiri::HTML.parse(html)
     noko.css('a').each do |link|
       link[:rel] = 'nofollow'
     end
-    noko.to_html.html_safe
+    (noko / "body").inner_html.html_safe
   end
 
   def escape_js_string(str)
