4525dba3293317ed97a7b8e628ebc091a012473f
Vitor Bittencourt <vitorhp2@gmail.com>
2015-04-19 23:07:56 -0300

    Changed serve_static_assets to serve_static_files

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -10,7 +10,7 @@ Juvia::Application.configure do
   config.cache_classes = true
 
   # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.static_cache_control = "public, max-age=3600"
 
   # Show full error reports and disable caching
